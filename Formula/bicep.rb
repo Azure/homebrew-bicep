6efa2112fa13ca000e24000c95fabb50fc7a120d
Miguel D. Salcedo <misalcedo@github.com>
2022-03-23 16:10:38 -0400

    Add support for x86 64-bit linux

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -1,14 +1,22 @@
 class Bicep < Formula
+  EXECUTABLE = if RUBY_PLATFORM =~ /x86_64-darwin/
+    "bicep-osx-x64"
+  elsif RUBY_PLATFORM =~ /x86_64-linux/
+    "bicep-linux-x64"
+  end
+
+  VERSION = "0.4.1272"
+  
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
-  version "0.4.1272"
-  url "https://github.com/Azure/bicep/releases/download/v0.4.1272/bicep-osx-x64"
+  version VERSION
+  url "https://github.com/Azure/bicep/releases/download/v#{VERSION}/#{EXECUTABLE}"
   sha256 "d832e827224759f6eaf20019ef322832e16666ccb2b6cc8f1840a807cc527e49"
 
   license "MIT"
 
   def install
-    bin.install "bicep-osx-x64" => "bicep"
+    bin.install EXECUTABLE => "bicep"
   end
 
   test do
