59ba96db676d9e8565cf0145d7db0698e0ed7113
shenglol <shenglol@users.noreply.github.com>
2023-04-05 02:46:30 +0000

    Update Homebrew to v0.16.1

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -1,9 +1,9 @@
 class Bicep < Formula
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
-  version "0.15.31"
-  url "https://github.com/Azure/bicep/releases/download/v0.15.31/bicep-osx-x64"
-  sha256 "b86a5b945879a45ddccfc4f599d1a2827274d668d1d74deb4f5aa65cbf059ac6"
+  version "0.16.1"
+  url "https://github.com/Azure/bicep/releases/download/v0.16.1/bicep-osx-x64"
+  sha256 "b2d50c8256139ddd6e41561ae7c6c889b0aa9317c2141bc57ea45344fe34fb14"
 
   license "MIT"
 
