6a9974a261c6fb42f61bff1574e7518e31884578
Jonny Eskew <jeskew@users.noreply.github.com>
2025-07-07 15:46:14 -0400

    Update Homebrew to v0.36.177 (#81)

--- Formula/bicep.rb
+++ Formula/bicep.rb
@@ -1,15 +1,15 @@
 class Bicep < Formula
   desc "Bicep: next generation template language for Azure Resource Manager (ARM)"
   homepage "https://github.com/Azure/bicep"
-  version "0.36.1"
+  version "0.36.177"
 
   case
   when OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/Azure/bicep/releases/download/v0.36.1/bicep-osx-x64"
-    sha256 "ac7da4da427374a0035f208318216008a71db85535cf5b78d66bb14f9f2d6107"
+    url "https://github.com/Azure/bicep/releases/download/v0.36.177/bicep-osx-x64"
+    sha256 "d6a5388d23d3796bdb2d775980c950688486483b1ed315b9e2307f87dac6899b"
   when OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/Azure/bicep/releases/download/v0.36.1/bicep-osx-arm64"
-    sha256 "36c1a589173f6d35746a6544a4a4ee6511fbea69a4e1b4f85baf7b1582b2687f"
+    url "https://github.com/Azure/bicep/releases/download/v0.36.177/bicep-osx-arm64"
+    sha256 "f89d7ae86952e22d9e7c48d627160614d1990c67fbdb0bbdd5e9db8c3477ae38"
   else
     odie "Unexpected platform!"
   end
